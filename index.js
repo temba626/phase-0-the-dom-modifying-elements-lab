9795e1d27e90766c20e428178eb1944ecc43ccc7
Brandon Temba <brandontemba560@gmail.com>
2022-06-07 11:51:14 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code here!
+const testVar = "hello"
+
+/* code to remove the node element */
+document.querySelector("main#main").remove();
+
+/* new element here and assign it to newHeader */
+const newHeader = document.createElement("h1");
+newHeader.id = "victory"
+newHeader.innerHTML = "Temba is the champion!";
+newHeader.className = "victory";
